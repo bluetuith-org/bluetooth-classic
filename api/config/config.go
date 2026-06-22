7c0d6185d91c2cf9157249be36a0c7043a6e4b40
darkhz <kmachanwenw@gmail.com>
2026-06-22 15:26:43 +0530

    api: config: Add library path option

--- api/config/config.go
+++ api/config/config.go
@@ -11,11 +11,14 @@ const (
 
 // Configuration describes a general configuration.
 type Configuration struct {
-	// SocketPath holds the path to the socket used to interface with the shim.
+	// SocketPath holds the user-defined path to the socket used to interface with the 'haraltd' daemon.
 	SocketPath string
 
 	// AuthTimeout holds the timeout for authentication requests.
 	AuthTimeout time.Duration
+
+	// LibraryPath holds the custom user-defined path for the 'libhbluetooth' library.
+	LibraryPath string
 }
 
 // New returns a new configuration with the default authentication timeout.
